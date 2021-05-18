9a2f1d39d78b48dd5a0725917f70a0b92f8b5b18
valmsmith39a <valmsmith39a@gmail.com>
2021-05-17 20:32:55 -0700

    basic routes

--- lesson-1/app.py
+++ lesson-1/app.py
@@ -2,8 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # 'http://www.google.com/'
-def home():
-    return "Hello, world!"
+stores = [{
+    'name': 'My Wonderful Store',
+    'items': [{ 'name': 'My Item', 'price': 15.99}]
+}]
+
+@app.route('/store', method=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<strong:name>')
+def get_store(name): 
+    pass
+
+@app.route('/store'):
+    pass
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
